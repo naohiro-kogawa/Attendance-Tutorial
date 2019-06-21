850ae6df8be18a7e4975f97562ef88e42add2613
naohiro-kogawa <sakuraxsakuraxis@gmail.com>
2019-06-21 09:23:02 +0000

    department9.1.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190620132105) do
+ActiveRecord::Schema.define(version: 20190621085958) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190620132105) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin"
+    t.string "department"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
