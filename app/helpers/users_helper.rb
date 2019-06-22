abceb3a78141194746125600009c7791106b63f2
naohiro-kogawa <sakuraxsakuraxis@gmail.com>
2019-06-22 05:31:04 +0000

    Finish add-basic-info

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  # 勤怠基本情報を指定のフォーマットで返します。  
+  def format_basic_info(time)
+    format("%.2f", ((time.hour * 60) + time.min) / 60.0)
+  end
 end
+
+
+# 1つめの"%.2f"では、戻り値となる文字列書式を設定
+# 2つめの((time.hour * 60) + time.min) / 60.0は、フォーマットするための値を計算
